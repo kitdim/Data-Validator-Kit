a2fac118cce938886fddf2e68de830237e1e79e2
kitdim <kitaykin9808@gmail.com>
2023-10-07 16:40:40 +0300

    Rename

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SchemaTest {
+public class ValidatorTest {
     private Validator validator;
     @BeforeEach
     void initValidator() {
