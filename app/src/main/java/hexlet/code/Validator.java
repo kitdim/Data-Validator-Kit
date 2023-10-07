ad99a543ce7ab609a3e103cd2d39a9a3d351945a
kitdim <kitaykin9808@gmail.com>
2023-10-07 16:17:08 +0300

    update Validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -5,6 +5,10 @@ import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 public class Validator {
+    public Validator() {
+
+    }
+
     public StringSchema string() {
         return new StringSchema();
     }
