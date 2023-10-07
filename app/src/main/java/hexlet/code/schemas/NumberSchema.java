8160015bccbe6830fae4590dc89c8568de3b902f
kitdim <kitaykin9808@gmail.com>
2023-10-07 14:55:53 +0300

    add NumberSchema

---
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -0,0 +1,33 @@
+package hexlet.code.schemas;
+
+import java.util.Objects;
+
+public class NumberSchema extends BaseSchema {
+    public NumberSchema() {
+        super();
+        addCheck("instanceof", value -> {
+            if (value != null) {
+                return value instanceof Number;
+            }
+            return true;
+        });
+    }
+
+    public NumberSchema required() {
+        addCheck("required",
+                Objects::nonNull);
+        return this;
+    }
+
+    public NumberSchema positive() {
+        addCheck("positive",
+                value -> (value == null || ((Integer) value) >= 0));
+        return this;
+    }
+
+    public NumberSchema range(int start, int end) {
+        addCheck("range",
+                value -> (value == null || ((Integer) value) >= start && ((Integer) value) <= end));
+        return this;
+    }
+}
