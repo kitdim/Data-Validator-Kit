4be8a18076d3d1103bdf58515a351bc6c192d977
kitdim <kitaykin9808@gmail.com>
2023-10-07 14:56:18 +0300

    update StringSchema

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,19 +1,33 @@
 package hexlet.code.schemas;
 
-public class StringSchema {
-    public boolean isValid(String value) {
-        return value == null || value.isEmpty() || value.length() > 0;
+public class StringSchema extends BaseSchema {
+
+    public StringSchema() {
+        super();
+        addCheck("instanceof",
+                value -> {
+                    if (value != null) {
+                        return value instanceof String;
+                    }
+                    return true;
+                });
     }
 
     public StringSchema required() {
+        addCheck("required",
+                value -> (value != null && !value.equals("")));
         return this;
     }
 
     public StringSchema contains(String word) {
+        addCheck("contains",
+                value -> value != null && ((String) value).contains(word));
         return this;
     }
 
     public StringSchema minLength(int size) {
+        addCheck("minLength",
+                value -> value != null && ((String) value).length() >= size);
         return this;
     }
 }
